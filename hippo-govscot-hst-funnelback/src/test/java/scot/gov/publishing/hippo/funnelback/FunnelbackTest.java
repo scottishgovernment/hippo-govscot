d22d9c5637d1ae7b3bd7be62727c04fcd3800ccd
Martin Ellis <martin.ellis@gov.scot>
2022-06-27 16:16:55 +0100

    Add empty test to ensure test report gets generated

---
+++ hippo-govscot-hst-funnelback/src/test/java/scot/gov/publishing/hippo/funnelback/FunnelbackTest.java
@@ -0,0 +1,12 @@
+package scot.gov.publishing.hippo.funnelback;
+
+import org.junit.Test;
+
+public class FunnelbackTest {
+    
+    @Test
+    public void noop() {
+        // Ensure test report is generated
+    }
+
+}
