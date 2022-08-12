cb572d4f2e97bb9223e494b0c6ee41f3d0ee350e
Martin Ellis <martin.ellis@gov.scot>
2022-08-12 14:04:39 +0100

    MGS-6913 Add valve to include value of X-User-Type header in page cache key

---
+++ hippo-govscot-hst/src/main/java/scot/gov/publishing/hippo/hst/request/UserTypeValve.java
@@ -0,0 +1,43 @@
+package scot.gov.publishing.hippo.hst.request;
+
+import org.hippoecm.hst.container.valves.AbstractOrderableValve;
+import org.hippoecm.hst.core.container.ContainerException;
+import org.hippoecm.hst.core.container.PageCacheKey;
+import org.hippoecm.hst.core.container.ValveContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Adds the value of the X-User-Type header to the page cache key.
+ * This valve also makes the value available as a request attribute.
+ */
+public class UserTypeValve extends AbstractOrderableValve {
+
+    public static final String USERTYPE_REQUEST_ATTR_NAME =
+            UserTypeValve.class.getName() + ".userType";
+
+    static final String USERTYPE_CACHE_ATTR_NAME =
+            UserTypeValve.class.getName() + ".userType";
+
+    private static final String USERTYPE_HEADER = "X-User-Type";
+
+    @Override
+    public void invoke(ValveContext valveContext) throws ContainerException {
+        addUserTypeToCacheKey(valveContext);
+        valveContext.invokeNext();
+    }
+
+    private void addUserTypeToCacheKey(ValveContext valveContext) {
+        PageCacheKey pageCacheKey = valveContext
+                .getPageCacheContext()
+                .getPageCacheKey();
+
+        HttpServletRequest request = valveContext.getServletRequest();
+        String userType = request.getHeader(USERTYPE_HEADER);
+        if (userType != null) {
+            request.setAttribute(USERTYPE_REQUEST_ATTR_NAME, userType);
+            pageCacheKey.setAttribute(USERTYPE_CACHE_ATTR_NAME, userType);
+        }
+    }
+
+}
---
+++ hippo-govscot-hst/src/test/java/scot/gov/publishing/hippo/hst/request/UserTypeValveTest.java
@@ -0,0 +1,72 @@
+package scot.gov.publishing.hippo.hst.request;
+
+import org.hippoecm.hst.core.container.PageCacheContext;
+import org.hippoecm.hst.core.container.PageCacheKey;
+import org.hippoecm.hst.core.container.ValveContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class UserTypeValveTest {
+
+    private UserTypeValve valve = new UserTypeValve();
+
+    private ValveContext context = mock(ValveContext.class);
+
+    private PageCacheContext pageCacheContext= mock(PageCacheContext.class);
+
+    private PageCacheKey pageCacheKey = mock(PageCacheKey.class);
+
+    private MockHttpServletRequest req = new MockHttpServletRequest();
+
+    private MockHttpServletResponse resp = new MockHttpServletResponse();
+
+    @Before
+    public void setUp() {
+        when(context.getServletRequest()).thenReturn(req);
+        when(context.getPageCacheContext()).thenReturn(pageCacheContext);
+        when(pageCacheContext.getPageCacheKey()).thenReturn(pageCacheKey);
+    }
+
+    @Test
+    public void handlesInternalTraffic() throws Exception {
+        req.addHeader("X-User-Type", "internal");
+        valve.invoke(context);
+        InOrder inOrder = inOrder(pageCacheKey, context);
+        // Sets cache key
+        inOrder.verify(pageCacheKey).setAttribute(
+                eq(UserTypeValve.USERTYPE_CACHE_ATTR_NAME),
+                eq("internal"));
+        inOrder.verify(context).invokeNext();
+        // Sets request attribute
+        assertThat(req.getAttribute(UserTypeValve.USERTYPE_REQUEST_ATTR_NAME)).isEqualTo("internal");
+    }
+
+    @Test
+    public void handlesExternalTraffic() throws Exception {
+        req.addHeader("X-User-Type", "external");
+        valve.invoke(context);
+        InOrder inOrder = inOrder(pageCacheKey, context);
+        // Sets cache key
+        inOrder.verify(pageCacheKey).setAttribute(
+                eq(UserTypeValve.USERTYPE_CACHE_ATTR_NAME),
+                eq("external"));
+        inOrder.verify(context).invokeNext();
+        // Sets request attribute
+        assertThat(req.getAttribute(UserTypeValve.USERTYPE_REQUEST_ATTR_NAME)).isEqualTo("external");
+    }
+
+    @Test
+    public void handlesLocalDevelopmentTrafficInCargo() throws Exception {
+        valve.invoke(context);
+        verify(pageCacheKey, never()).setAttribute(anyString(), any());
+        verifyNoInteractions(pageCacheKey);
+        assertThat(req.getAttribute(UserTypeValve.USERTYPE_REQUEST_ATTR_NAME)).isNull();
+    }
+
+}
