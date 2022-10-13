2017f0c64b75ff61373c8b758b1ba1133423a243
David Sinclair <david.sinclair@gov.scot>
2022-10-13 14:39:53 +0100

    Move Search component to hippo-govscot

---
+++ hippo-govscot-hst-funnelback/src/main/java/scot/gov/publishing/hippo/funnelback/component/SearchComponent.java
@@ -0,0 +1,43 @@
+package scot.gov.publishing.hippo.funnelback.component;
+
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.request.ComponentConfiguration;
+import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
+
+import javax.servlet.ServletContext;
+
+/**
+ * Component suitable for the base of a search page.
+ */
+public class SearchComponent extends EssentialsContentComponent {
+
+    private String searchType;
+
+    public void init(ServletContext servletContext, ComponentConfiguration componentConfig) {
+        super.init(servletContext, componentConfig);
+        this.searchType = componentConfig.getRawParameters().getOrDefault("searchtype", "resilient");
+    }
+
+    public void doBeforeRender(HstRequest request, HstResponse response) {
+        super.doBeforeRender(request, response);
+
+        // each page has a designated search type, and then there are the search settings for the site.
+        // the specific funnelback and bloomreach pages are for testing and sholdhave their auto complete
+        // value hard coded.
+        //
+        // otherwise the value is determined from the search settings document
+        switch (searchType) {
+            case "bloomreach":
+                request.setAttribute("autoCompleteEnabled", false);
+                break;
+            case "funnelback":
+                request.setAttribute("autoCompleteEnabled", true);
+                break;
+            default:
+                // defer to the type specific in search settings
+                request.setAttribute("autoCompleteEnabled", ResilientSearchComponent.searchSettings().isEnabled());
+        }
+    }
+
+}
