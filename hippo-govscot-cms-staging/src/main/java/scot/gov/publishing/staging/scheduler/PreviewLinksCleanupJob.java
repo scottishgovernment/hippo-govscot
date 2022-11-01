32ba8b732433d5f03923892ea23d890a0a2c7373
David Sinclair <david.sinclair@gov.scot>
2022-11-01 16:07:51 +0000

    MGS-6751 add preview links cleanup job

---
+++ hippo-govscot-cms-staging/src/main/java/scot/gov/publishing/staging/scheduler/PreviewLinksCleanupJob.java
@@ -0,0 +1,89 @@
+package scot.gov.publishing.staging.scheduler;
+
+import org.hippoecm.repository.util.JcrUtils;
+import org.onehippo.repository.scheduling.RepositoryJob;
+import org.onehippo.repository.scheduling.RepositoryJobExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import java.util.Calendar;
+
+public class PreviewLinksCleanupJob implements RepositoryJob {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PreviewLinksCleanupJob.class);
+
+    private static final String CONFIG_BATCH_SIZE = "batchsize";
+
+    private static final String PREVIEW_LINKS_QUERY = "//element(*, staging:preview)";
+
+    @Override
+    public void execute(RepositoryJobExecutionContext context) throws RepositoryException {
+        LOG.info("Running preview links cleanup job");
+        Session session = context.createSystemSession();
+        try {
+            long batchSize = batchSize(context);
+            removeExpiredPreviewLinks(batchSize, session);
+        } finally {
+            session.logout();
+        }
+    }
+
+    long batchSize(RepositoryJobExecutionContext context) {
+        long batchSize;
+        try {
+            batchSize = Long.parseLong(context.getAttribute(CONFIG_BATCH_SIZE));
+        } catch (NumberFormatException e) {
+            LOG.warn("Incorrect batch size '{}'. Setting default to 100", context.getAttribute(CONFIG_BATCH_SIZE));
+            batchSize = 100;
+        }
+        return batchSize;
+    }
+
+    private void removeExpiredPreviewLinks(final long batchSize, final Session session) throws RepositoryException {
+        QueryManager queryManager = session.getWorkspace().getQueryManager();
+        Query query = queryManager.createQuery(PREVIEW_LINKS_QUERY, Query.XPATH);
+        NodeIterator nodes = query.execute().getNodes();
+        int count = 0;
+
+        while (nodes.hasNext()) {
+            Node node = nodes.nextNode();
+            if (removeIfExpired(node)) {
+                count++;
+            }
+
+            if (count++ % batchSize == 0) {
+                session.save();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RepositoryException("Interupted cleaning old preview links", e);
+                }
+            }
+        }
+        if (session.hasPendingChanges()) {
+            session.save();
+        }
+        if (count > 0) {
+            LOG.info("Done cleaning {} items", count);
+        } else {
+            LOG.info("No timed out items");
+        }
+    }
+
+    boolean removeIfExpired(Node node) throws RepositoryException {
+        Calendar expirationCalendar = JcrUtils.getDateProperty(node, "staging:expirationdate", null);
+        if(expirationCalendar == null || Calendar.getInstance().before(expirationCalendar)){
+            return true;
+        }
+        LOG.debug("Removing preview node at {}", node.getPath());
+        node.remove();
+        return false;
+    }
+
+}
