f1938d8fe4423b54986dbe204d812df73433fe7e
David Sinclair <david.sinclair@gov.scot>
2023-11-20 14:31:48 +0000

    MGS-7476 add bulkpublish module

---
+++ hippo-govscot-bulkpublish-cms/src/main/java/scot/gov/publishing/bulkpublish/ScheduleBulkDialog.java
@@ -0,0 +1,27 @@
+package scot.gov.publishing.bulkpublish;
+
+import java.util.Date;
+
+import org.apache.wicket.model.IModel;
+import org.hippoecm.addon.workflow.IWorkflowInvoker;
+import org.hippoecm.addon.workflow.WorkflowDialog;
+import org.hippoecm.frontend.dialog.Dialog;
+import org.hippoecm.frontend.plugins.reviewedactions.dialogs.DatePickerComponent;
+import org.hippoecm.repository.api.WorkflowDescriptor;
+
+public class ScheduleBulkDialog extends WorkflowDialog<WorkflowDescriptor> {
+
+    public ScheduleBulkDialog(IWorkflowInvoker invoker, IModel<WorkflowDescriptor> nodeModel,
+                              IModel<Date> dateModel, IModel<String> titleModel,
+                              IModel<String> notification, IModel<String> action,
+                              IModel<String> dateText) {
+        super(invoker, nodeModel, titleModel);
+
+        setCssClass("hippo-workflow-dialog");
+        setNotification(notification);
+        setOkLabel(action);
+        setFocusOnOk();
+
+        addOrReplace(new DatePickerComponent(Dialog.BOTTOM_LEFT_ID, dateModel, dateText));
+    }
+}
