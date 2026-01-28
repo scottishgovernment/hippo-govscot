408f344a85033bad9fccdb7ee9fadc3da1a764df
David Sinclair <david.sinclair@gov.scot>
2026-01-28 14:43:14 +0000

    MGS-8182 make custom tooltip rendering to shared code

---
+++ hippo-govscot-cms/src/main/java/scot/gov/publishing/plugins/ExtendedEditorPlugin.java
@@ -0,0 +1,20 @@
+package scot.gov.publishing.plugins;
+
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnLoadHeaderItem;
+import org.hippoecm.frontend.editor.editor.EditorPlugin;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+
+public class ExtendedEditorPlugin extends EditorPlugin {
+
+    public ExtendedEditorPlugin(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+
+        response.render(OnLoadHeaderItem.forScript("$(\".hippo-hint-field\").each(function() {if(!$(this).next().hasClass('hint-text')){$(this).hide();$(this).after(\"<p class='hint-text'>\"+$(this).attr(\"data-original-title\") +\"</p>\");}});"));
+    }
+}
